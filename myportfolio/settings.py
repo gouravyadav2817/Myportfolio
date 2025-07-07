f939370611f98204c8457ec0b8c3814c96bea516
gouravyadav2817 <ygourav334@gmail.com>
2025-07-07 14:25:13 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -116,10 +116,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+from pathlib import Path
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = '/static/'  # 🔁 Should start and end with a slash
-STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
