6f40f8ca83325aa2a5d2aff33160f58e8b7ee90d
gouravyadav2817 <ygourav334@gmail.com>
2025-07-07 14:43:09 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
