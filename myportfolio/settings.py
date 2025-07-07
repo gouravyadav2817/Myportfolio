a39b22bd7d3112ff22531ecd1c7840341d5ce563
gouravyadav2817 <ygourav334@gmail.com>
2025-07-07 18:49:19 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -77,7 +77,7 @@ USE_TZ = True
 
 # ✅ Static file settings
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR/ 'portfolio' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
